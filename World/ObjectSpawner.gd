fca9f754192f19b7d5d5a6a8a5ac90a3da25711c
Caroline Jeffra <caroline.jeffra@outlook.com>
2023-03-07 18:26:32 +0100

    Commit for code changes during lecture 35. dumpsters

--- World/ObjectSpawner.gd
+++ World/ObjectSpawner.gd
@@ -8,6 +8,7 @@ var number_of_billboards = 75
 var number_of_traffic_cones = 40
 var number_of_hydrants = 50
 var number_of_streetlights = 50 # the more lights and shadows we have, the slower the game
+var number_of_ramps = 25
 
 # handle generation and placement of props in game map
 func generate_props(tile_list, size):
@@ -29,9 +30,9 @@ func random_tile(tile_count):
 		selected_tiles.append(tile)
 	return selected_tiles
 
-# handles putting the cars as parked props in the scene
+# handles putting the cars parked as props in the scene
 func place_cars():
-	var tile_list = random_tile(number_of_parked_cars)
+	var tile_list = random_tile(number_of_parked_cars + number_of_ramps)
 	for i in range(number_of_parked_cars):
 		var tile = tile_list[0]
 		var tile_type = get_node("..").get_cell_item(tile.x, 0, tile.z)
@@ -44,6 +45,8 @@ func place_cars():
 			rpc("spawn_cars", tile, tile_rotation)
 		# remove the completed tile from the list of tiles for placing prop
 		tile_list.pop_front()
+	# call the place ramp function and send the list of remaining available tiles to it
+	place_ramps(tile_list)
 
 # handles spawning the cars
 sync func spawn_cars(tile, car_rotation):
@@ -55,6 +58,32 @@ sync func spawn_cars(tile, car_rotation):
 	car.rotation_degrees.y = car_rotation
 	# add the translated and rotated prop instance to the scene on the correct tile
 	add_child(car, true)
+
+# handles putting the dumpster ramps as props in the scene
+func place_ramps(tile_list):
+	for i in range(number_of_ramps):
+		var tile = tile_list[0]
+		var tile_type = get_node("..").get_cell_item(tile.x, 0, tile.z)
+		# send the tile type we have a look up possible rotations for the props
+		var allowed_rotations = $ObjectRotLookup.lookup_rotation(tile_type)
+		if not allowed_rotations == null:
+			var tile_rotation = allowed_rotations[randi() % allowed_rotations.size() -1] * -1
+			tile.y = tile.y + 1 # adjustment for height of prop as needed
+			# remote call to spawn prop 
+			rpc("spawn_ramps", tile, tile_rotation)
+		# remove the completed tile from the list of tiles for placing prop
+		tile_list.pop_front()
+
+# handles spawning the dumpster ramps
+sync func spawn_ramps(tile, ramp_rotation):
+	# preload the prop scene instance for faster loading
+	var ramp = preload("res://Props/Dumpster/Dumpster.tscn").instance()
+	# Vector3 coordinates involve converting to tile size of 20 and then adding 10 to offset origin to center for each
+	ramp.translation = Vector3((tile.x * 20) + 10, tile.y, (tile.z * 20) + 10)
+	# rotate the prop instance according to the rotation parameter
+	ramp.rotation_degrees.y = ramp_rotation
+	# add the translated and rotated prop instance to the scene on the correct tile
+	add_child(ramp, true)
 
 # handles putting the billboards in the scene
 func place_billboards():
