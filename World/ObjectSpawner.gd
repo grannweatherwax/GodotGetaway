5ffafeb4357843a97a6608c098d05fbdaf2a225c
Caroline Jeffra <caroline.jeffra@outlook.com>
2023-03-07 12:47:28 +0100

    Commit for code changes during lecture 34. street lights

--- World/ObjectSpawner.gd
+++ World/ObjectSpawner.gd
@@ -7,6 +7,7 @@ var number_of_parked_cars = 100
 var number_of_billboards = 75
 var number_of_traffic_cones = 40
 var number_of_hydrants = 50
+var number_of_streetlights = 50 # the more lights and shadows we have, the slower the game
 
 # handle generation and placement of props in game map
 func generate_props(tile_list, size):
@@ -16,7 +17,7 @@ func generate_props(tile_list, size):
 	place_billboards()
 	place_traffic_cones()
 	place_hydrants()
-	
+	place_streetlights()
 
 # handles returning a randomized list of an appropriate length for prop placement
 func random_tile(tile_count):
@@ -125,7 +126,7 @@ func place_hydrants():
 		# remove the completed tile from the list of tiles for placing props
 		tile_list.pop_front()
 
-# handles spawning the traffic cones
+# handles spawning the hydrants
 sync func spawn_hydrants(tile, hydrant_rotation):
 	# preload the prop scene instance for faster loading
 	var hydrants = preload("res://Props/Hydrant/Hydrant.tscn").instance()
@@ -135,3 +136,30 @@ sync func spawn_hydrants(tile, hydrant_rotation):
 	hydrants.rotation_degrees.y = hydrant_rotation
 	# add the translated and rotated prop instance to the scene on the correct tile
 	add_child(hydrants, true)
+
+# handles putting the streetlamps in the scene
+func place_streetlights():
+	var tile_list = random_tile(number_of_streetlights)
+	for i in range(number_of_streetlights):
+		var tile = tile_list[0]
+		var tile_type = get_node("..").get_cell_item(tile.x, 0, tile.z)
+		# send the tile type we have a look up possible rotations for the props
+		var allowed_rotations = $ObjectRotLookup.lookup_rotation(tile_type)
+		if not allowed_rotations == null:
+			var tile_rotation = allowed_rotations[randi() % allowed_rotations.size() -1] * -1
+			tile.y = tile.y + 1 # adjustment for height of prop spawn point as needed
+			# remote call to spawn prop 
+			rpc("spawn_streetlights", tile, tile_rotation)
+		# remove the completed tile from the list of tiles for placing props
+		tile_list.pop_front()
+
+# handles spawning the streetlamps
+sync func spawn_streetlights(tile, streetlight_rotation):
+	# preload the prop scene instance for faster loading
+	var streetlight = preload("res://Props/StreetLight/StreetLight.tscn").instance()
+	# Vector3 coordinates involve converting to tile size of 20 and then adding 10 to offset origin to center for each
+	streetlight.translation = Vector3((tile.x * 20) + 10, tile.y, (tile.z * 20) + 10)
+	# rotate the props instance according to the rotation parameter
+	streetlight.rotation_degrees.y = streetlight_rotation
+	# add the translated and rotated prop instance to the scene on the correct tile
+	add_child(streetlight, true)
